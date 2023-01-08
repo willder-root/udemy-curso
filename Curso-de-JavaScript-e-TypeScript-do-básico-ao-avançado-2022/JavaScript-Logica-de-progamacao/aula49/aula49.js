f50d3c00dfb7ac38e7c19e9dec543d2e6a0ae36b
redlliw <willderaraujo_99_gds@hotmail.com>
2023-01-08 18:28:08 -0300

    aula49.js

---
+++ Curso-de-JavaScript-e-TypeScript-do-básico-ao-avançado-2022/JavaScript-Logica-de-progamacao/aula49/aula49.js
@@ -0,0 +1,26 @@
+falaoi()
+
+
+
+
+const obj
+
+
+
+
+
+
+
+
+
+
+function falaoi() {
+    console.log("oi")    
+}
+
+
+const falaOiNovamente = function () {
+    console.log("oi novamente")
+}
+
+falaOiNovamente()
